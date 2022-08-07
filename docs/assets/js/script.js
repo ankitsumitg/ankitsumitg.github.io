f2a50ab1b6e4e0055bcf7674fe3077e65e813351
Ankit Gupta <ankitsumitg@gmail.com>
2022-08-07 23:52:34 +0530

    Removed jquery dependency and shifted to vanilla java script

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,17 +1,11 @@
-$( ".inner-switch" ).on("click", function(){
-    if( $( "body" ).hasClass( "dark" )) {
-      $( "body" ).removeClass( "dark" );
-      $( ".inner-switch" ).text( "🌑" );
-      $( ".mode" ).text( "Go Dark :" );
-    } else {
-      $( "body" ).addClass( "dark" );
-      $( ".inner-switch" ).text( "🔆" );
-      $( ".mode" ).text( "Unleash Light :" );
-    }
-});
-
-$( document ).ready(function() {
-    $( "body" ).addClass( "dark" );
-    $( ".inner-switch" ).text( "🔆" );
-    $( ".mode" ).text( "Unleash Light :" );
+document.querySelector( ".inner-switch" ).addEventListener("click", function(){
+  if( document.querySelector( "body" ).classList.contains( "dark" )) {
+    document.querySelector( "body" ).classList.remove( "dark" );
+    document.querySelector( ".inner-switch" ).text( "🌑" );
+    document.querySelector( ".mode" ).text( "Go Dark :" );
+  } else {
+    document.querySelector( "body" ).classList.add( "dark" );
+    document.querySelector( ".inner-switch" ).text( "🔆" );
+    document.querySelector( ".mode" ).text( "Unleash Light :" );
+  }
 });
